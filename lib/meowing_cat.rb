3edb343102c0e6ad92072ca95cad83c8e82c5203
Dalia Sawaya <dalia.sawaya@flatironschool.com>
2020-07-22 17:39:43 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,18 @@
-## code your solution here. 
+require 'pry'
+class Cat
+
+  attr_accessor :name
+  
+  #getter & setter
+  #reader & writer
+  #setters/writers methods that allow you to define the value of an attribute, i.e. to set, or re-set it. e.g. to set a name, or a birth date or a breed
+  
+  #getters/readers methods that allow you to read, or retrieve, or see, or get to, or use, an attribute of an object
+
+  def meow
+    puts "meow!"
+  end
+  
+  binding.pry
+  
+end
